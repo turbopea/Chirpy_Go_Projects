db34e53f80a73a37c04362dcdffb81d61ec275d9
Eriks <erikszirnis11@gmail.com>
2026-08-21 18:31:19 +0300

    Chirpy project. Created bunch of handlers, each handler has it's own purpose

--- main.go
+++ main.go
@@ -1,18 +1,134 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"sync/atomic"
 )
 
 func main() {
-	servemux := http.NewServeMux()
-	s := servemux.Handle("/", http.FileServer(http.Dir(".")))
-
-	s := &http.Server{
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /api/healthz", healthCheck)
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	apiCfg := &apiConfig{}
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.reset)
+	mux.HandleFunc("POST /api/validate_chirp", validate_chirp)
+	server := &http.Server{
 		Addr:    ":8080",
-		Handler: s,
+		Handler: mux,
+	}
+	log.Fatal(server.ListenAndServe())
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+func (cfg *apiConfig) metrics(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	x := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html`, x)))
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+
+		w.Write([]byte(fmt.Sprintf("HTTP method %d not allowed", r.Method)))
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+
+}
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Reset to 0"))
+}
+
+func validate_chirp(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
+		w.WriteHeader(500)
+		return
+	}
+	if len(params.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		w.WriteHeader(400)
+		return
+	}
+	badWords := badWords()
+	textArray := strings.Split(params.Body, ` `)
+	for _, badWord := range badWords {
+		for _, word := range textArray {
+			if word == badWord {
+				word = "****"
+			}
+		}
+	}
+	respondWithJSON(w, 200, returnVals{
+		Valid: true,
+	})
+}
+func respondWithError(w http.ResponseWriter, code int, msg string) error {
+	return respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(code)
+	w.Write(response)
+	return nil
+}
+
+func badWords() []string {
+
+	listofWords := []string{
+		"kerfuffle",
+		"sharbert",
+		"fornax",
 	}
 
-	log.Fatal(s.ListenAndServe())
+	return listofWords
 }
